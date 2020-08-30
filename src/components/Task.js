01535af2e60422e2a48d57796e205e2531f51972
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-08-30 18:35:47 +0530

    modified task.js

--- src/components/Task.js
+++ src/components/Task.js
@@ -2,12 +2,11 @@ import React from 'react';
 
 const Task = ({ task, onClick, id }) => {
   const colors = ['notDone', 'doing', 'done'];
-  const color = colors[task.taskId];
   const content = task.taskId === 2 ? 'lineThrough content' : 'content';
 
   return (
     <div className="display">
-      <span className={color}></span>
+      <span className={colors[task.taskId]}></span>
       <span className={content} onClick={() => onClick(id)}>
         {task.content}
       </span>
