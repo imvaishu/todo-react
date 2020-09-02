a81e5bd6fbdcc522b0e250b29ef5ec8862abfc93
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-09-02 13:11:10 +0530

    Added mark of remove in task

--- src/components/Task.js
+++ src/components/Task.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
-const Task = ({ task, onClick, id }) => {
+const Task = ({ task, onClick, handleRemove }) => {
   const classes = `display ${task.status}`;
   return (
-    <div className={classes}>
-      <div></div>
-      <span className='content' onClick={() => onClick(id)}>
-        {task.content}
-      </span>
+    <div className="item">
+      <div className={classes}>
+        <div></div>
+        <span className="content" onClick={() => onClick(task.id)}>
+          {task.content}
+        </span>
+        <div className="remove" onClick={() => handleRemove(task.id)}>
+          X
+        </div>
+      </div>
     </div>
   );
 };
