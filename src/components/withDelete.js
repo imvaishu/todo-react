8e7baf602a772c05a13d85d0b8aa956e633cd588
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-09-05 18:27:47 +0530

    Added hoc component withDelete

---
+++ src/components/withDelete.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const WithDelete = function (Component) {
+  return function (props) {
+    console.log(Component);
+    return (
+      <div className="item">
+        <Component {...props} />
+        <div className="remove" onClick={() => props.handleRemove(props.task)}>
+          X
+        </div>
+      </div>
+    );
+  };
+};
+
+WithDelete.defaultProps = {
+  task: {},
+};
+
+export default WithDelete;
