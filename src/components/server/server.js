00f9b55e6287a26767aecc2c4f33dfadff346dc2
vaishnavi <58027647+imvaishu@users.noreply.github.com>
2020-09-08 19:55:54 +0530

    separated handlers from server.js

--- src/components/server/server.js
+++ src/components/server/server.js
@@ -1,53 +1,27 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { getDefault, toggleStatus } = require('../toggle');
+const {
+  getTodoList,
+  createTodoItem,
+  deleteTodoList,
+  updateTitle,
+  updateTodoStatus,
+  deleteTodo,
+} = require('./handlers');
 
-let index = { lastId: 0 };
-
-let todoList = {
-  title: 'Todo',
-  todo: [{ content: 'hello', status: getDefault(), id: index.lastId }],
-};
 app.use(bodyParser.json());
 
-app.get('/api/getTodoList', (req, res) => {
-  res.json(todoList);
-});
+app.get('/api/getTodoList', getTodoList);
 
-app.post('/api/createTodoItem', (req, res) => {
-  const id = ++index.lastId;
-  todoList.todo.push({
-    content: req.body.content,
-    status: getDefault(),
-    id: id,
-  });
-  id.lastId = id;
-  res.json(todoList.todo);
-});
+app.post('/api/createTodoItem', createTodoItem);
 
-app.post('/api/deleteTodo/:id', (req, res) => {
-  todoList.todo = todoList.todo.filter(todo => todo.id !== +req.params.id);
-  res.json(todoList.todo);
-});
+app.post('/api/deleteTodo/:id', deleteTodo);
 
-app.post('/api/updateTodoStatus/:id', (req, res) => {
-  todoList.todo.forEach(todo => {
-    if (todo.id === +req.params.id) {
-      todo.status = toggleStatus(todo.status);
-    }
-  });
-  res.json(todoList.todo);
-});
+app.post('/api/updateTodoStatus/:id', updateTodoStatus);
 
-app.post('/api/updateTitle', (req, res) => {
-  todoList.title = req.body.title;
-  res.json(todoList.title);
-});
+app.post('/api/updateTitle', updateTitle);
 
-app.get('/api/deleteTodoList', (req, res) => {
-  todoList = { title: 'Todo', todo: [] };
-  res.json(todoList);
-});
+app.get('/api/deleteTodoList', deleteTodoList);
 
 app.listen(8000, () => console.log('listening on 8000'));
